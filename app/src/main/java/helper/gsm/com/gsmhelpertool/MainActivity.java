1d96669ca284ca543217cd5c764bd812d8588092
Sadiq <sadiqodho@gmail.com>
2019-05-17 04:30:48 +0500

    Changed application according to lastest android API level functionalty. Made the changes for BroadCastReceivers

--- app/src/main/java/helper/gsm/com/gsmhelpertool/MainActivity.java
+++ app/src/main/java/helper/gsm/com/gsmhelpertool/MainActivity.java
@@ -1,11 +1,22 @@
 package helper.gsm.com.gsmhelpertool;
 
 import android.Manifest;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private final static Integer NOT_SUPPORTED_VERSION = 8;
+    private SMSSender smsSender;
+    private MySMSReceiver mySMSReceiver;
+    private IntentFilter intentFilter;
+    private IntentFilter intentFilterRec;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +31,34 @@ public class MainActivity extends AppCompatActivity {
                         Manifest.permission.SEND_SMS},
                 1);
 
+        smsSender = new SMSSender();
+        intentFilter = new IntentFilter("helper.gsm.com.gsmhelpertool");
+
+        mySMSReceiver = new MySMSReceiver();
+        intentFilterRec = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
+
+        Button b1 = (Button) findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = (EditText) findViewById(R.id.editText);
+                String phone = editText.getText().toString();
+                Intent intent = new Intent();
+                intent.setAction("helper.gsm.com.gsmhelpertool");
+                intent.putExtra("phone", phone);
+                intent.putExtra("message", phone);
+                sendBroadcast(intent);
+            }
+        });
+
+        registerReceiver(smsSender, intentFilter);
+        registerReceiver(mySMSReceiver, intentFilterRec);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(smsSender);
+        unregisterReceiver(mySMSReceiver);
     }
 }
